f8839122211eb11ed90d615e80091013efab043a
nikhilkuyya <nikhilkuyya@gmail.com>
2018-10-20 20:57:44 +0530

    configuration correcting : for webpack-dev-server and babel configuation

--- .babelrc.js
+++ .babelrc.js
@@ -2,7 +2,12 @@ const isTest = String(process.env.NODE_ENV) === "test";
 module.exports = {
   presets: ["@babel/preset-react"],
   plugins: [
-    "@babel/plugin-transform-modules-commonjs",
+    [
+      "@babel/plugin-transform-modules-commonjs",
+      {
+        modules: isTest
+      }
+    ],
     "@babel/plugin-proposal-optional-chaining"
   ].filter(Boolean)
 };
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,4 +1,7 @@
-const path = require("path");
+const {
+  resolve,
+  join
+} = require("path");
 const webpackMerge = require("webpack-merge");
 const cleanwebpackPlugin = require("clean-webpack-plugin");
 
@@ -7,49 +10,48 @@ const frameworkConfig = framework =>
 const presetsConfig = env =>
   require(`./build-utils/presets/webpack.${env.presets}`)(env);
 
-const myCustomPlugin = require("./build-utils/plugins/optional.chaining.webpackPlugin");
+// test plugin creation.
+// const myCustomPlugin = require("./build-utils/plugins/optional.chaining.webpackPlugin");
 
 module.exports = env => {
-  let commonConfig = {
-    entry: "./src/js/app.js",
+  console.log(__dirname, 'dir')
+  const commonConfig = {
+    context: join(__dirname, "src", "js"),
+    entry: "./app.js",
     output: {
-      path: path.join(__dirname, "dist/"),
+      path: join(__dirname, "dist"),
       filename: "index.js",
-      publicPath: path.join(__dirname, "dist/"),
+      publicPath: env.mode === "developement" ? "/dist/" : join(__dirname, "dist/"),
       chunkFilename: "[name].lazy.chunk.js"
     },
     mode: (env && env.mode) || "none",
     module: {
-      rules: [
-        {
-          test: /\.(html)$/,
-          use: {
-            loader: "html-loader"
-          }
+      rules: [{
+        test: /\.(html)$/,
+        use: {
+          loader: "html-loader"
         }
-      ]
+      }]
     },
-    plugins: [new myCustomPlugin(), new cleanwebpackPlugin(["dist"])]
+    plugins: [new cleanwebpackPlugin(["dist"])]
   };
 
   const frameworkMergeConfig = webpackMerge(
     commonConfig,
     frameworkConfig("elm"),
     frameworkConfig("react"),
-    frameworkConfig("vue")
+    frameworkConfig("vue"),
+    frameworkConfig("react"),
   );
   let finalConfig = frameworkMergeConfig;
   if (env !== undefined && env.presets !== undefined) {
     if (env.presets === "chart") {
-      finalConfig = webpackMerge(
-        {},
-        frameworkConfig("react"),
+      finalConfig = webpackMerge({},
         presetsConfig(env)
       );
     } else {
       finalConfig = webpackMerge({}, frameworkMergeConfig, presetsConfig(env));
     }
   }
-  console.log(finalConfig);
   return finalConfig;
 };
